8995df683e3de142df2071c9a490124417822b28
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2025-02-06 15:39:50 -0300

    init: Início do projeto.

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,25 @@
+package principal;
+
+
+import controle.CtrCliente;
+
+/**
+ * Classe que possui a operação main da aplicação.
+ *
+ * Serve para dar início ao sistema.
+ *
+ * @author osmarbraz
+ */
+public class Principal {
+
+    /**
+     * Inicia a aplicação.
+     *
+     * @param args args
+     */
+    public static void main(String[] args) {
+
+        CtrCliente controle = new CtrCliente();
+        controle.executar();
+    }
+}
---
+++ src/test/java/cliente/TestCliente.java
@@ -0,0 +1,37 @@
+package cliente;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import entidade.Cliente;
+
+class TestCliente {
+
+    /**
+     * Testa o construtor sem argumentos do cliente.
+     */
+    @Test
+    void testCliente() {
+        Cliente instancia = new Cliente();
+        assertTrue("".equals(instancia.getClienteId()) && "".equals(instancia.getNome()) && "".equals(instancia.getCpf()));
+    }
+
+    @Test
+    void testClienteIdInt() {
+        Cliente instancia = new Cliente();
+        instancia.setClienteId(1);
+        assertTrue("1".equals(instancia.getClienteId()) && "".equals(instancia.getNome()) && "".equals(instancia.getCpf()));
+    }
+
+    @Test
+    void testParaString() {
+        Cliente instancia = new Cliente();
+        String esperado = "clienteId: - Nome : - CPF :";
+        assertEquals(esperado, instancia.toString());
+    }    
+}
